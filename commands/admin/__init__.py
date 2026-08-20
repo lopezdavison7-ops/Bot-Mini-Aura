a89d38fe7f20ff26bb3fa10edea461d439a7e3a4
BostHost <lopezdavison7@gmail.com>
2026-08-20 17:55:41 -0600

    Crear admin/__init__.py

---
+++ commands/admin/__init__.py
@@ -0,0 +1,6 @@
+from .kick import ejecutar as kick
+from .ban import ejecutar as ban
+from .promote import ejecutar as promote
+from .demote import ejecutar as demote
+from .group import ejecutar as group
+from .welcome import ejecutar as welcome
