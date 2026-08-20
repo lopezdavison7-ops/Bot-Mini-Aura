d7b1db4fa72cb460aab2189a8b10fa271b9f975f
BostHost <lopezdavison7@gmail.com>
2026-08-20 17:24:45 -0600

    Actualizar __init__.py

--- commands/fun/__init__.py
+++ commands/fun/__init__.py
@@ -1,1 +1,5 @@
-# Comandos de [fun]
+from .chiste import ejecutar as chiste
+from .dato import ejecutar as dato
+from .frase import ejecutar as frase
+from .amor import ejecutar as amor
+from .futuro import ejecutar as futuro
